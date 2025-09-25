18c92381b80868c9713dfe5ea4b74977c9f643eb
matteocappelletti98-creator <matteocappelletti98@gmail.com>
2025-09-25 09:40:37 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Nav from "@/components/Nav";
 import Index from "@/pages/Index";
 import PlacesPage from "@/pages/PlacesPage";
 import AddPlace from "@/pages/AddPlace";
+import BlogIndex from "./pages/BlogIndex";
+import BlogPost from "./pages/BlogPost";
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
         <Route path="/places" element={<PlacesPage />} />
         <Route path="/add-place" element={<AddPlace />} />
         <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/blog" element={<BlogIndex />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   );
